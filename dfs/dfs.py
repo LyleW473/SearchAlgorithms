f2df6a06090797ba643d2ade1411ddfa025df7ba
Lyle Wong <wlyle473@gmail.com>
2024-01-01 20:27:17 +0000

    Changes made: - Added function to create a tree using randomly generated values (and coinflips so nodes can have either 0, 1 or 2 children). - Fixed DFS algorithm (was not printing out the order correctly in all cases). - Removed redundant parameters in tree construction algorithms.

--- dfs/dfs.py
+++ dfs/dfs.py
@@ -1,5 +1,6 @@
-from tree import build_tree
+from tree import *
 from node import Node
+from random import seed as random_seed
 
 def dfs(node, order):
 
@@ -9,8 +10,14 @@ def dfs(node, order):
     order.append(node.val)
     dfs(node.left, order = order)
     dfs(node.right, order = order)
-    return order
 
-my_tree = build_tree(0, node = Node(val = 5), low = 0, high = 10, vals_used = set([5]))
-order = dfs(i = 0, node = my_tree, order = [])
-print(f"Traversal order: {order}")
+tree1 = build_tree(node = Node(val = 5), low = 0, high = 10, vals_used = set([5]))
+order1 = []
+dfs(node = tree1, order = order1)
+print(f"Traversal order 1: {order1}")
+
+random_seed(2004)
+tree2 = build_random_tree(node = Node(val = 5), num_nodes_in_tree = 0, num_nodes_limit = 20, vals_used = set([5]))
+order2 = []
+dfs(node = tree2, order = order2)
+print(f"Traversal order 2: {order2}")
--- dfs/tree.py
+++ dfs/tree.py
@@ -1,6 +1,7 @@
 from node import Node
+from random import randint
 
-def build_tree(i, node, low, high, vals_used):
+def build_tree(node, low, high, vals_used):
 
     if not node:
         return node
@@ -8,12 +9,33 @@ def build_tree(i, node, low, high, vals_used):
     # Left child
     if node.val > low and node.val - 1 not in vals_used:
         vals_used.add(node.val - 1)
-        node.left = build_tree(i + 1, node = Node(val = node.val - 1), low = low, high = high, vals_used = vals_used)
+        node.left = build_tree(node = Node(val = node.val - 1), low = low, high = high, vals_used = vals_used)
         vals_used.remove(node.val - 1)
     # Right child
     if node.val < high and node.val + 1 not in vals_used:
         vals_used.add(node.val + 1)
-        node.right = build_tree(i + 1, node = Node(val = node.val + 1), low = low, high = high, vals_used = vals_used)
+        node.right = build_tree(node = Node(val = node.val + 1), low = low, high = high, vals_used = vals_used)
         vals_used.remove(node.val + 1)
+    
+    return node
+
+def build_random_tree(node, num_nodes_in_tree, num_nodes_limit, vals_used):
+    
+    if not node or num_nodes_in_tree == num_nodes_limit:
+        return node
+    
+    # Generate random values
+    random_val_1 = randint(-50, 50)
+    random_val_2 = randint(-50, 50)
+
+    # Add values generated
+    vals_used.add(random_val_1)
+    vals_used.add(random_val_2)
+
+    # Build left and right child nodes (Coinflip to generate left and right child)
+    if randint(0, 1) == 0:
+        node.left = build_random_tree(node = Node(val = random_val_1), num_nodes_in_tree = num_nodes_in_tree + 1, num_nodes_limit = num_nodes_limit, vals_used = vals_used)
+    if randint(0, 1) == 1:
+        node.right = build_random_tree(node = Node(val = random_val_2), num_nodes_in_tree = num_nodes_in_tree + 1, num_nodes_limit = num_nodes_limit, vals_used = vals_used)
     
     return node
